d081aae65b59f1f8bbf12104c32f82c94f2d0602
phamtiendat2911 <94462043+phamtiendat2911@users.noreply.github.com>
2021-12-06 15:57:31 +0700

    QLyKTX

---
+++ QuanLyKyTucXa/fDoiMatKhau.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKyTucXa
+{
+    public partial class fDoiMatKhau : Form
+    {
+        private fDangNhap login;
+        public fDoiMatKhau(fDangNhap login)
+        {
+            InitializeComponent();
+            this.login = login;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            login.setName(txtMKmoi.Text);
+            this.Hide();
+            login.Show();
+        }
+    }
+}
